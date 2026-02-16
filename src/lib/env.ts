61b760117278ad08a0c2edce0d8bf01f64949cc4
0xprasanth <raviprasanth45@gmail.com>
2026-02-17 00:35:41 +0530

    feat: admin seeeder

---
+++ src/lib/env.ts
@@ -0,0 +1,17 @@
+import { createEnv } from '@t3-oss/env-nextjs'
+import { z } from 'zod'
+
+export const env = createEnv({
+    server: {
+        DATABASE_URL: z.url(),
+        CMS_SEED_ADMIN_EMAIL: z.email(),
+        CMS_SEED_ADMIN_PASSWORD: z.string().min(1),
+    },
+    client: {},
+
+    runtimeEnv: {
+        DATABASE_URL: process.env.DATABASE_URL,
+        CMS_SEED_ADMIN_EMAIL: process.env.CMS_SEED_ADMIN_EMAIL,
+        CMS_SEED_ADMIN_PASSWORD: process.env.CMS_SEED_ADMIN_PASSWORD,
+    },
+})
